d9b7a11edb36491bdc80b9d409e902f04b04a4c9
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-10-17 18:59:52 +0900

    :recycle: 各設定ファイルを最適化

---
+++ src/app/next-theme-provider.tsx
@@ -0,0 +1,8 @@
+'use client'
+
+import { ThemeProvider } from 'next-themes'
+import * as React from 'react'
+
+export function NextThemeProvider({ children }: { children: React.ReactNode }) {
+  return <ThemeProvider attribute='class'>{children}</ThemeProvider>
+}
