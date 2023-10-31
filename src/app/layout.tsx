967f1849a385af4cf378026ff93b55f6fb0c9c07
UniUrchin <85057870+UniUrchin@users.noreply.github.com>
2023-10-31 10:31:39 +0900

    :lipstick: Next/fontをChakraに適用

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import { Noto_Sans_JP } from 'next/font/google';
 import { Container } from './container';
 import { Providers } from './providers';
 
@@ -8,18 +7,13 @@ export const metadata: Metadata = {
   description: '', // TODO: いい感じのDescriptionを考えておく
 };
 
-const NotoSansJP = Noto_Sans_JP({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
-
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="jp">
       <head>
         <meta name="viewport" content="width=device-width,initial-scale=1" />
       </head>
-      <body className={NotoSansJP.className}>
+      <body>
         <Providers>
           <Container>{children}</Container>
         </Providers>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,16 @@
 import { extendTheme } from '@chakra-ui/react';
+import { Noto_Sans_JP } from 'next/font/google';
+
+const NotoSansJP = Noto_Sans_JP({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
 
 export const theme = extendTheme({
+  fonts: {
+    heading: NotoSansJP,
+    body: NotoSansJP,
+  },
   colors: {
     theme: {
       primary: {
